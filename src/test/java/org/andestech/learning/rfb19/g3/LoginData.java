eb793bf724e45560480f1e5fa55119fec90ffdd0
a0z9 <a0z9@rambler.ru>
2019-03-05 18:02:07 +0300

    5 --loginTest

---
+++ src/test/java/org/andestech/learning/rfb19/g3/LoginData.java
@@ -0,0 +1,26 @@
+package org.andestech.learning.rfb19.g3;
+
+public class LoginData {
+
+    private String login;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    private String password;
+
+    public LoginData(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+
+
+
+
+}
