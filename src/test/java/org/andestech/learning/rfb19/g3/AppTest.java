699c506c183d23ee338caadc897f281572b329a7
a0z9 <a0z9@rambler.ru>
2019-03-05 17:01:31 +0300

    2

--- src/test/java/org/andestech/learning/rfb19/g3/AppTest.java
+++ src/test/java/org/andestech/learning/rfb19/g3/AppTest.java
@@ -3,6 +3,7 @@ package org.andestech.learning.rfb19.g3;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogEntry;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
@@ -12,6 +13,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.nio.file.Files;
 import java.util.Date;
 import java.util.logging.Level;
 
@@ -21,6 +23,7 @@ public class AppTest
 {
     private WebDriver wd = null;
     private DesiredCapabilities cap = null;
+    private ChromeOptions options = null;
 
     @BeforeClass
     public void initData(){
@@ -38,25 +41,32 @@ public class AppTest
         logPrefs.enable(LogType.PERFORMANCE, Level.ALL);
         cap.setCapability(CapabilityType.LOGGING_PREFS, logPrefs);
 
+        options = new ChromeOptions();
+        options.merge(cap);
+
     }
 
     @Test
     public void testCaseChrome01()
     {
 
-
-
-        wd = new ChromeDriver(cap);
-       // wd.get("http://newsru.com");
+      //  wd = new ChromeDriver(cap);
+        wd = new ChromeDriver(options);
+        wd.get("http://google.com");
 
         wd.manage().logs().getAvailableLogTypes().forEach(t -> System.out.println(t));
 
-//        wd.manage().logs().get("browser").getAll().
-//                forEach( (LogEntry logEntry) ->
-//
-//        System.out.printf("%s, level:%s, mess:%s \n",
-//                new Date(logEntry.getTimestamp()),
-//                logEntry.getLevel(), logEntry.getMessage())                   );
+     //   wd.manage().logs().get("browser").getAll().
+     //           forEach( (LogEntry logEntry) ->
+
+           wd.manage().logs().get("performance").getAll().
+                   forEach( (LogEntry logEntry) ->
+
+        System.out.printf("%s, level:%s, mess:%s \n",
+                new Date(logEntry.getTimestamp()),
+                logEntry.getLevel(), logEntry.getMessage())                   );
+
+      //  Files.write(P)
 
 
         assertTrue( true );
